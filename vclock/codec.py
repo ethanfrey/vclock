d5b5ae41d8480c9893f02f1623d0e19a61be1aba
Ethan <ethan@orderbird.com>
2016-02-22 19:39:01 +0100

    Add new base62 codec

--- vclock/codec.py
+++ vclock/codec.py
@@ -1,28 +1,34 @@
-from base64 import b64encode, b64decode
-import struct
-
-
 class ArrayCodec(object):
     """
     This is a default encoding to pack the data. Designed for best storage in ascii.
     You can use a custom encoder for eg. larger data sizes, more felxibility, etc.
-    Currently, this handles arrays with no keys, and max 2**8 ids and 2**24 counts
+    Currently, this handles arrays with no keys, and max 2**8 ids and 62**4 counts.
+
+    Originally used base64 encoding, but then we discovered that the encoding of 53 < encoding of 1.
+    So, our own "base 62" encoding that is actually maintains order of the value when comparing
+    the encoded strings
     """
     COUNT_BYTES = 4
+    BASE = 62;
+    DIGITS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    REVERSE = {digit: idx for idx, digit in enumerate(DIGITS)}
 
     def encode_count(self, big):
-        """This encodes a large integer < 2*24 ~= 16 million, into 4 ascii characters"""
-        binary = struct.pack(">I", big)
-        # py2 - returns b'\x00', py3 - returns 0
-        assert binary[0] in (b'\x00', 0)
-        binary = binary[1:]
-        return b64encode(binary)
+        """This encodes a large integer < 62**4 ~= 14.7 million, into 4 ascii characters"""
+        result = ''
+        for i in range(self.COUNT_BYTES):
+            mod = big % self.BASE
+            result = self.DIGITS[mod] + result
+            big = big // self.BASE
+        return result
 
     def decode_count(self, ascii):
         """This decodes an integer encoded by encode_counter"""
-        binary = b'\x00' + b64decode(ascii)
-        big = struct.unpack(">I", binary)[0]
-        return big
+        total = 0
+        for digit in ascii:
+            total *= self.BASE
+            total += self.REVERSE[digit]
+        return total
 
     def encode_vector(self, vector):
         """Encodes a vector array as a string"""
@@ -40,7 +46,7 @@ class DictCodec(ArrayCodec):
     of key-value pairs (int -> int). It will be somewhat larger when most actors participate,
     but the gain is that when most actors do not touch the item, the clock is much smaller.
     """
-    COUNT_BYTES = 6
+    KEY_BYTES = 2
 
     def __init__(self, int_keys=True):
         self.int_keys = int_keys
@@ -71,7 +77,7 @@ class DictCodec(ArrayCodec):
 
     def decode_vector(self, line):
         """Decodes a vector string into a generator (wrap in list() to instantiate)"""
-        n = self.COUNT_BYTES
+        n = self.COUNT_BYTES + self.KEY_BYTES
         result = {}
         for i in range(0, len(line), n):
             ekey, eval = line[i:i+2], line[i+2:i+n]
